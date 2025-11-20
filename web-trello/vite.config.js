ef4529aea76661d7ea8003bb549a1bd207d50a12
Nilva Lisex Bonilla Gutierrez <nbg0008@alu.medac.es>
2025-11-20 13:18:07 +0100

    mas config

--- web-trello/vite.config.js
+++ web-trello/vite.config.js
@@ -5,13 +5,12 @@ const environment = process.env.NODE_ENV || "development";
 const host = environment == "production" ? "0.0.0.0" : "localhost";
 const port = process.env.PORT || 3000;
 
-console.log("Config for %s environment. Host: %s, Port: %s", environment, host, port);
+console.info("Config for %s environment. Host: %s, Port: %s", environment, host, port);
 
 export default defineConfig({
   plugins: [tailwindcss()],
   server: { 
     host: host,
-    port: port ,
-
+    port: port 
   } // 👈 correrá en http://localhost:3000
 })
