e3595cd151f3898882e544e9060302fba7ea43fc
Nilva Lisex Bonilla Gutierrez <nbg0008@alu.medac.es>
2025-11-20 12:49:33 +0100

    mas cambio de config

--- web-trello/vite.config.js
+++ web-trello/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
-const host = import.meta.env.PROD ? "0.0.0.0" : "localhost";
-const port = import.meta.env.PORT || 3000;
+const environment = process.env.NODE_ENV || "development";
+const host = environment == "production" ? "0.0.0.0" : "localhost";
+const port = process.env.PORT || 3000;
 
-console.log("Config for %s environment. Host: %s, Port: %s", 
-  import.meta.env.PROD ? "PROD":"DEV", host, port);
+console.log("Config for %s environment. Host: %s, Port: %s", environment, host, port);
 
 export default defineConfig({
   plugins: [tailwindcss()],
