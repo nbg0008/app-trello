171bff951992661e0518829d13d7c08327f01a30
Nilva Lisex Bonilla Gutierrez <nbg0008@alu.medac.es>
2025-11-20 12:28:26 +0100

    otro intento de arreglar config de host y port

--- web-trello/vite.config.js
+++ web-trello/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
+const host = import.meta.env.PROD ? "0.0.0.0" : "localhost";
+const port = import.meta.env.PORT || 3000;
+
+console.log("Config for %s environment. Host: %s, Port: %s", 
+  import.meta.env.PROD ? "PROD":"DEV", host, port);
+
 export default defineConfig({
   plugins: [tailwindcss()],
   server: { 
-    host: "0.0.0.0",
-    port: process.env.PORT || 3000 ,
+    host: host,
+    port: port ,
 
   } // 👈 correrá en http://localhost:3000
 })
