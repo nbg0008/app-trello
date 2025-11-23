0970883b38cb71bb8c2d3322f3cdb5ce5d0c0b8b
Nilva Lisex Bonilla Gutierrez <nbg0008@alu.medac.es>
2025-11-23 21:19:18 +0100

    habilitar endpoints para healthcheck en backend, y cambio en config de vite para habilitar https

--- web-trello/vite.config.js
+++ web-trello/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import mkcert from "vite-plugin-mkcert";
+
 
 const environment = process.env.NODE_ENV || "development";
 const host = environment == "production" ? "0.0.0.0" : "localhost";
@@ -8,7 +10,7 @@ const port = process.env.PORT || 3000;
 console.info("Config for %s environment. Host: %s, Port: %s", environment, host, port);
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), mkcert()],
   server: { 
     host: host,
     port: port 
