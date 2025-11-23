111891c42aa6f8550340ed21b12d0c4cf54ec713
Nilva Lisex Bonilla Gutierrez <nbg0008@alu.medac.es>
2025-11-23 21:24:26 +0100

    elimminada config de vite para habilitar https

--- web-trello/vite.config.js
+++ web-trello/vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import mkcert from "vite-plugin-mkcert";
-
 
 const environment = process.env.NODE_ENV || "development";
 const host = environment == "production" ? "0.0.0.0" : "localhost";
@@ -10,7 +8,7 @@ const port = process.env.PORT || 3000;
 console.info("Config for %s environment. Host: %s, Port: %s", environment, host, port);
 
 export default defineConfig({
-  plugins: [tailwindcss(), mkcert()],
+  plugins: [tailwindcss()],
   server: { 
     host: host,
     port: port 
