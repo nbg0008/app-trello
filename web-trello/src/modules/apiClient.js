13329fbfb384b359884249ea85545274397c5262
Nilva Lisex Bonilla Gutierrez <nbg0008@alu.medac.es>
2025-11-23 21:58:31 +0100

    cambio forma de acceder a variables de entorno

--- web-trello/src/modules/apiClient.js
+++ web-trello/src/modules/apiClient.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  "http://${import.meta.env.VITE_API_BASE_URL}" || "http://localhost:8080/trello/v1"; 
+  "http://${process.env.VITE_API_BASE_URL}" || "http://localhost:8080/trello/v1"; 
 
 export async function apiFetch(path, options = {}) {
   const token = localStorage.getItem("token");
--- web-trello/src/modules/authClient.js
+++ web-trello/src/modules/authClient.js
@@ -1,5 +1,5 @@
 const API_URL = 
-  "http://${import.meta.env.VITE_API_BASE_URL}" || "http://localhost:8080/trello/v1";
+  "http://${process.env.VITE_API_BASE_URL}" || "http://localhost:8080/trello/v1";
 const TOKEN_STORAGE_KEY = "token";
 const REFRESH_TOKEN_STORAGE_KEY = "refreshToken";
 const SESSION_STORAGE_KEY = "trello_auth_session";
